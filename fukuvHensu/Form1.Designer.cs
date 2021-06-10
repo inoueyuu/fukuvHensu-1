5f691e894f52f16315f6816e0b70cffa5d46f8a9
ookamiyoujyo <ookamiyoujyo@gmail.com>
2021-06-10 12:18:54 +0900

    Initial commit

--- fukuvHensu/Form1.Designer.cs
+++ fukuvHensu/Form1.Designer.cs
@@ -31,8 +31,6 @@ namespace fukuvHensu
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -43,27 +41,11 @@ namespace fukuvHensu
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 70);
+            this.label1.Location = new System.Drawing.Point(329, 111);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(10, 227);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 19);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "0";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 303);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 19);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "0";
             // 
             // button1
             // 
@@ -119,11 +101,10 @@ namespace fukuvHensu
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,8 +113,6 @@ namespace fukuvHensu
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -12,6 +12,10 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+
+        int vx = -30;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,32 +23,55 @@ namespace fukuvHensu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += int.Parse(textBox1.Text);
-            label1.Top += int.Parse(textBox2.Text);
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if(label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top<0)
+            {
+                vy = -Math.Abs(vy);
+            }
+            if(label1.Right> ClientSize.Width)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Bottom> ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "-5";
+            vx = 0;
+            vy = -5;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-5";
-            textBox2.Text = "0";
+            vx = -5;
+            vy = 0;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "5";
+            vx = 0;
+            vy = 5;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "5";
-            textBox2.Text = "0";
+            vx = 5; 
+            vy = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // MessageBox.Show(""+ ClientSize.Width);
+            //MessageBox.Show(""+ ClientSize.Height);
         }
     }
 }
